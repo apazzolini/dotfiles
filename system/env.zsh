fa362f61356f08bf94fedaea6c802ce3a62d5daa
Andre Azzolini <apazzolini@gmail.com>
2012-03-01 16:35:58 -0600

    Migrated previous oh-my-zsh config to one that doesn't use it.

--- system/env.zsh
+++ system/env.zsh
@@ -23,4 +23,7 @@ PROMPT='%{$reset_color%}%{${fg[green]}%}%10~ $(git_prompt_info)%{${fg_bold[$CARE
 RPROMPT='[%*] ${return_code}'
 ZSH_THEME_GIT_PROMPT_PREFIX="%{$fg[yellow]%}‹"
 ZSH_THEME_GIT_PROMPT_SUFFIX="› %{$reset_color%}"
+ZSH_THEME_GIT_PROMPT_DIRTY="*"              # Text to display if the branch is dirty
+ZSH_THEME_GIT_PROMPT_CLEAN=""               # Text to display if the branch is clean
+
 
