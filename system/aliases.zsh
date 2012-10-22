960de71ab25dd9bf21fc4c490fd7ff242ae889d0
Andre Azzolini <apazzolini@gmail.com>
2012-10-22 10:59:29 -0500

    vim tweaks, aliases, rbenv

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -17,16 +17,22 @@ alias hg='history 1 -1 | grep '
 alias pg='ps -A | grep '
 alias psa='ps -A'
 alias ka='killall'
+alias pgj='ps -Aef | grep java'
+alias kaj='killall java'
 
 # open current window in path finder
 alias o='open -a "Path Finder.app" .'
 
 # atea (https://github.com/pkamenarsky/atea)
-alias t='vim /Users/Andre/Dropbox/Random/tasks.txt'
+alias t='mvim /Users/Andre/Dropbox/Random/tasks.txt'
 
 # copies public ssh key to clipboard
 alias pubkey="more ~/.ssh/id_dsa.pub | pbcopy | echo '=> Public key copied to pasteboard.'"
 
 # starts up a webserver at the current directory
-alias webserver="python -m SimpleHTTPServer 9090"
+alias httpserver="python -m SimpleHTTPServer 9090"
+
+# use hub to front git
+alias git='hub'
+
 
