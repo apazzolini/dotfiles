0e2df2a53db88488885f3c9e1dd2581e581e94ce
Andre Azzolini <apazzolini@gmail.com>
2013-09-13 09:39:29 -0500

    track this under git

---
+++ system/rsync.zsh
@@ -0,0 +1,5 @@
+# sync phone
+alias rspics='rsync -avu iphone:/User/Media/DCIM/101APPLE/ ~/Dropbox/iPhone/photos'
+alias rstext='rsync -avu iphone:/private/var/mobile/Library/SMS ~/Dropbox/iPhone/texts'
+alias rspicswork='rsync -avu iphone-work:/User/Media/DCIM/101APPLE/ ~/Dropbox/iPhone/photos'
+alias rstextwork='rsync -avu iphone-work:/private/var/mobile/Library/SMS ~/Dropbox/iPhone/texts'
