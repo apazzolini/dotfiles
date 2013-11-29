c8c0cb89ac8a3bddd1e943e252e849dee9b5a99a
Andre Azzolini <apazzolini@gmail.com>
2013-11-29 13:13:54 -0600

    Update rsync aliases

--- system/rsync.zsh
+++ system/rsync.zsh
@@ -1,5 +1,5 @@
 # sync phone
-alias rspics='rsync -avu iphone:/User/Media/DCIM/101APPLE/ ~/Dropbox/iPhone/photos'
-alias rstext='rsync -avu iphone:/private/var/mobile/Library/SMS ~/Dropbox/iPhone/texts'
-alias rspicswork='rsync -avu iphone-work:/User/Media/DCIM/101APPLE/ ~/Dropbox/iPhone/photos'
-alias rstextwork='rsync -avu iphone-work:/private/var/mobile/Library/SMS ~/Dropbox/iPhone/texts'
+alias rspics='rsync -avu iphone:/User/Media/DCIM/101APPLE/ ~/AeroFS/Photos/iPhone'
+alias rstext='rsync -avu iphone:/private/var/mobile/Library/SMS ~/AeroFS/iPhone/texts'
+alias rspicswork='rsync -avu iphone-work:/User/Media/DCIM/101APPLE/ ~/AeroFS/Photos/iPhone'
+alias rstextwork='rsync -avu iphone-work:/private/var/mobile/Library/SMS ~/AeroFS/iPhone/texts'
