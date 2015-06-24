c330a1a4f7b77b881f140581f73e46d07b9d8a34
Andre Azzolini <apazzolini@gmail.com>
2015-06-24 08:39:17 -0500

    Add docker config

---
+++ docker/boot2docker.zsh
@@ -0,0 +1,6 @@
+# The following export lines come from the shellinit command:
+# $(boot2docker shellinit)
+
+export DOCKER_HOST="tcp://`boot2docker ip`:2376"
+export DOCKER_CERT_PATH=/Users/Andre/.boot2docker/certs/boot2docker-vm
+export DOCKER_TLS_VERIFY=1
