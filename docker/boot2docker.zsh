9102d41b94bc730630f88c59bb2129afb68d2ea2
Andre Azzolini <apazzolini@gmail.com>
2015-08-04 20:52:55 -0500

    Disable docker commands

--- docker/boot2docker.zsh
+++ docker/boot2docker.zsh
@@ -1,7 +1,7 @@
 # The following export lines come from the shellinit command:
 # $(boot2docker shellinit)
-export DOCKER_HOST="tcp://`boot2docker ip`:2376"
-export DOCKER_CERT_PATH=/Users/Andre/.boot2docker/certs/boot2docker-vm
-export DOCKER_TLS_VERIFY=1
+#export DOCKER_HOST="tcp://`boot2docker ip`:2376"
+#export DOCKER_CERT_PATH=/Users/Andre/.boot2docker/certs/boot2docker-vm
+#export DOCKER_TLS_VERIFY=1
 
-alias d='docker'
+#alias d='docker'
