dabedd7614b145885533000931ece519030b8d0e
Andre Azzolini <apazzolini@gmail.com>
2015-10-03 02:13:59 -0500

    Fix rsync alias for correct folder

--- system/rsync.zsh
+++ system/rsync.zsh
@@ -1,3 +1,3 @@
 # sync phone
-alias rspics='rsync -avu iphone:/User/Media/DCIM/101APPLE/ ~/AeroFS/Photos/iPhone2'
-alias rstext='rsync -avu iphone:/private/var/mobile/Library/SMS ~/AeroFS/iPhone/texts2'
+alias rspics='rsync -avu iphone:/User/Media/DCIM/101APPLE/ ~/Air/Photos/iPhone2'
+alias rstext='rsync -avu iphone:/private/var/mobile/Library/SMS ~/Air/iPhone/texts2'
