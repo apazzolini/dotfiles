70e5c7e81b2c2fe776cb28d2bd8e294d4b684a99
Andre Azzolini <apazzolini@gmail.com>
2016-05-19 09:28:31 -0500

    Add ecstail function

--- system/docker.zsh
+++ system/docker.zsh
@@ -16,3 +16,10 @@ drmall() {
     d rmi $(d images -q)
 }
 
+alias dm='docker-machine'
+
+ecstail() {
+    short=`ssh ecs docker ps | grep $1 | awk '{print $1}'`
+    long=`ssh ecs docker inspect --format '{{.Id}}' $short`
+    ssh ecs sudo tail -fn1000 "/var/lib/docker/containers/$long/$long-json.log" | jq '.log|fromjson' -c --unbuffered | bunyan -o short $2
+}
