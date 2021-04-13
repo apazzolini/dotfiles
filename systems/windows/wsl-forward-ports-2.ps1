68ca52ef658ddf454eb4b9acaa7c60b91224a6c3
Andre Azzolini <andre@azzolini.io>
2021-04-12 21:51:33 -0500

    Update WSL scripts

---
+++ systems/windows/wsl-forward-ports-2.ps1
@@ -0,0 +1,43 @@
+#Tested with Windows 10 Pro version 1909 and WSL2
+
+#get the WSL ip address
+$connectIp = wsl ip addr show eth0
+$connectIp = $connectIp | Select-String -Pattern '(\.\d+){3}'
+
+#check if there is an IP
+$found = $connectIp -match '\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}';
+if( $found ){
+  $connectIp = $matches[0];
+} else{
+  echo "The Script Exited, the ip address of WSL 2 cannot be found";
+  exit;
+}
+
+#[Ports]
+#All the ports you want to forward separated by coma
+$ports=@(80,443,3308,8080,9090,2222);
+
+#[LOCAL IP] USE EITHER LINE 22 OR LINES 24 AND 25 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+#You can change the addr to your ip config to listen to a specific address
+#$listenIp = '0.0.0.0';
+
+# OR if it is not working you can detect it with these two lines
+$listenIp = Test-Connection -ComputerName (hostname) -Count 1  | Select -ExpandProperty IPV4Address
+$listenIp = $listenIp.IPAddressToString
+
+#create an array of the ports
+$ports_a = $ports -join ",";
+
+#Remove Firewall Exception Rules
+iex "Remove-NetFireWallRule -DisplayName 'WSL 2 Firewall Unlock' ";
+
+#adding Exception Rules for inbound and outbound Rules
+iex "New-NetFireWallRule -DisplayName 'WSL 2 Firewall Unlock' -Direction Outbound -LocalPort $ports_a -Action Allow -Protocol TCP";
+iex "New-NetFireWallRule -DisplayName 'WSL 2 Firewall Unlock' -Direction Inbound -LocalPort $ports_a -Action Allow -Protocol TCP";
+
+#remove and readd the ports to connect them with the wsl
+for( $i = 0; $i -lt $ports.length; $i++ ){
+  $port = $ports[$i];
+  iex "netsh interface portproxy delete v4tov4 listenport=$port listenaddress=$listenIp";
+  iex "netsh interface portproxy add v4tov4 listenport=$port listenaddress=$listenIp connectport=$port connectaddress=$connectIp";
+}
--- systems/windows/wsl-forward-ports.ps1
+++ systems/windows/wsl-forward-ports.ps1
@@ -1,43 +1,51 @@
-#Tested with Windows 10 Pro version 1909 and WSL2
+wsl sudo service ssh start
 
-#get the WSL ip address
-$connectIp = wsl ip addr show eth0
-$connectIp = $connectIp | Select-String -Pattern '(\.\d+){3}'
+# Display all portproxy information
+If ($Args[0] -eq "list") {
+    netsh interface portproxy show v4tov4;
+    exit;
+}
 
-#check if there is an IP
-$found = $connectIp -match '\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}';
-if( $found ){
-  $connectIp = $matches[0];
-} else{
-  echo "The Script Exited, the ip address of WSL 2 cannot be found";
+# If elevation needed, start new process
+If (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator))
+{
+  # Relaunch as an elevated process:
+  Start-Process powershell.exe "-File",('"{0}"' -f $MyInvocation.MyCommand.Path),"$Args runas" -Verb RunAs
+  exit
+}
+
+# You should modify '$Ports' for your applications
+$Ports = (2222,80,443,8080,3308,9090)
+
+# Check WSL ip address
+wsl hostname -I | Set-Variable -Name "WSL"
+$found = $WSL -match '\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}';
+if (-not $found) {
+  echo "WSL2 cannot be found. Terminate script.";
   exit;
 }
 
-#[Ports]
-#All the ports you want to forward separated by coma
-$ports=@(80,443,3308,8080,9090,2222);
+# Remove and Create NetFireWallRule
+Remove-NetFireWallRule -DisplayName 'WSL 2 Firewall Unlock';
+if ($Args[0] -ne "delete") {
+  New-NetFireWallRule -DisplayName 'WSL 2 Firewall Unlock' -Direction Outbound -LocalPort $Ports -Action Allow -Protocol TCP;
+  New-NetFireWallRule -DisplayName 'WSL 2 Firewall Unlock' -Direction Inbound -LocalPort $Ports -Action Allow -Protocol TCP;
+}
 
-#[LOCAL IP] USE EITHER LINE 22 OR LINES 24 AND 25 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-#You can change the addr to your ip config to listen to a specific address
-#$listenIp = '0.0.0.0';
+# Add each port into portproxy
+$Addr = "0.0.0.0"
+Foreach ($Port in $Ports) {
+    iex "netsh interface portproxy delete v4tov4 listenaddress=$Addr listenport=$Port | Out-Null";
+    if ($Args[0] -ne "delete") {
+        iex "netsh interface portproxy add v4tov4 listenaddress=$Addr listenport=$Port connectaddress=$WSL connectport=$Port | Out-Null";
+    }
+}
 
-# OR if it is not working you can detect it with these two lines
-$listenIp = Test-Connection -ComputerName (hostname) -Count 1  | Select -ExpandProperty IPV4Address
-$listenIp = $listenIp.IPAddressToString
+# Display all portproxy information
+netsh interface portproxy show v4tov4;
 
-#create an array of the ports
-$ports_a = $ports -join ",";
-
-#Remove Firewall Exception Rules
-iex "Remove-NetFireWallRule -DisplayName 'WSL 2 Firewall Unlock' ";
-
-#adding Exception Rules for inbound and outbound Rules
-iex "New-NetFireWallRule -DisplayName 'WSL 2 Firewall Unlock' -Direction Outbound -LocalPort $ports_a -Action Allow -Protocol TCP";
-iex "New-NetFireWallRule -DisplayName 'WSL 2 Firewall Unlock' -Direction Inbound -LocalPort $ports_a -Action Allow -Protocol TCP";
-
-#remove and readd the ports to connect them with the wsl
-for( $i = 0; $i -lt $ports.length; $i++ ){
-  $port = $ports[$i];
-  iex "netsh interface portproxy delete v4tov4 listenport=$port listenaddress=$listenIp";
-  iex "netsh interface portproxy add v4tov4 listenport=$port listenaddress=$listenIp connectport=$port connectaddress=$connectIp";
+# Give user to chance to see above list when relaunched start
+If ($Args[0] -eq "runas" -Or $Args[1] -eq "runas") {
+  Write-Host -NoNewLine 'Press any key to close! ';
+  $null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
 }
