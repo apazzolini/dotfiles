82f7242f4ec930693cd456923f6412c0c2b10c8e
Andre Azzolini <andre@azzolini.io>
2021-04-13 23:49:27 -0500

    Add rg function

---
+++ nvim/lua/andre/rg.lua
@@ -0,0 +1,49 @@
+local Job = require('plenary.job')
+
+local M = {}
+
+function M.rg(arg, cwd)
+  local entries = {}
+
+  local opts = {
+    command = '/opt/homebrew/bin/rg',
+    args = {
+      '--color=never',
+      '--follow',
+      '--no-heading',
+      '--with-filename',
+      '--line-number',
+      '--column',
+      '--smart-case',
+      arg,
+    },
+    on_stdout = function(_, data)
+      if cwd then
+        data = cwd .. '/' .. data
+      end
+
+      table.insert(entries, data)
+    end,
+  }
+
+  if cwd then
+    opts.cwd = cwd
+  end
+
+  local job = Job:new(opts)
+
+  job:after(vim.schedule_wrap(function(a,b)
+    vim.fn.setqflist({}, " ", {
+      title = cmd,
+      lines = entries,
+    })
+    vim.cmd [[copen]]
+  end))
+
+  job:start()
+end
+
+local opts = { noremap = true }
+vim.api.nvim_set_keymap('n', ',A', ':lua require("andre.rg").rg ', opts)
+
+return M
