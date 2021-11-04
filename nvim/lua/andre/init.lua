709d952719376ba8db63504381361d948a9ceec3
Andre Azzolini <andre@azzolini.io>
2021-11-04 00:00:20 -0500

    vimconf

--- nvim/lua/andre/init.lua
+++ nvim/lua/andre/init.lua
@@ -10,3 +10,8 @@ require('andre.octo')
 require('andre.telescope')
 require('andre.treesitter')
 require('andre.autopairs') -- this has to load after cmp
+
+local parsers = require('nvim-treesitter.parsers')
+local configs = parsers.get_parser_configs()
+local ft_str = table.concat(vim.tbl_map(function(ft) return configs[ft].filetype or ft end, parsers.available_parsers()), ',')
+vim.cmd('autocmd Filetype ' .. ft_str .. ' setlocal foldmethod=expr foldexpr=nvim_treesitter#foldexpr()')
