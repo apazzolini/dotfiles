8c3e004aad23102cf4328585b829bed9f06288f3
Andre Azzolini <andre@azzolini.io>
2022-07-18 10:51:33 -0500

    aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,10 +1,25 @@
-# ls aliases
 if [ `uname` = 'Darwin' ]
 then
   alias ls="gls -F --color"
+  alias trackpad='osascript ~/.dotfiles/bin/toggle-internal-trackpad.scpt'
+  alias o='open .'
+  alias pubkey="more ~/.ssh/id_rsa.pub | pbcopy | echo '=> Public key copied to pasteboard.'"
+  alias curdir="pwd | tr -d '\n' | pbcopy"
 else
   alias ls="ls -F --color"
+  alias o='explorer.exe .'
 fi
+
+if [[ `hostname` = andrembw* ]]
+then
+  alias dl='docker-compose -f /Users/andre/Work/docker/docker-compose.yml'
+  alias notes='IS_NOTES=1 nvim -O "/Users/andre/Work/notes/log.md"'
+else
+  alias dl='docker-compose -f /apps/docker-compose.yml'
+  alias notes='IS_NOTES=1 nvim -O "/Users/andre/Library/Mobile Documents/com~apple~CloudDocs/Wiki/index.md"'
+fi
+
+# ls aliases
 alias l="ls -lh --group-directories-first"
 alias ll="l -A"
 
@@ -22,7 +37,6 @@ alias k='kill -9'
 alias httpserver="python -m SimpleHTTPServer 9090"
 
 # git aliases
-alias st='stree'
 alias lg='lazygit -ucd ~/.config/lazygit'
 
 # shortcuts for npm/yarn
@@ -31,55 +45,29 @@ alias nr="npm run"
 alias pr="pnpm run"
 
 # tmux
-# alias t="tmux -u -2"
 alias t="tmux-sessionizer"
 alias ta="tmux -u -2 attach"
 alias td="tmux -u -2 detach"
 alias tks="tmux -u -2 kill-session"
-alias tawork="$HOME/.dotfiles/tmux/sessions/work.sh"
-alias tadre="$HOME/.dotfiles/tmux/sessions/dre.sh"
-
-if [[ `hostname` = andrembw* ]]
-then
-  alias notes='IS_NOTES=1 nvim -O "/Users/andre/Work/notes/log.md"'
-else
-  alias notes='IS_NOTES=1 nvim -O "/Users/andre/Library/Mobile Documents/com~apple~CloudDocs/Wiki/index.md"'
-fi
 
 # vim
-alias v=nvim
-alias vi=nvim
-alias vim=nvim
+alias v="nvim"
+alias vi="nvim"
+alias vim="nvim"
 alias vf='nvim "$(fzf)"'
 alias vrc='nvim -c "cd ~/.dotfiles" -- ~/.dotfiles/nvim/init.vim'
 alias vp='nvim package.json'
 
 # misc
-alias se='sudoedit'
 alias d='docker'
 alias da='d ps -a'
 alias j=z
 alias rg='rg -i'
 
 # work
-alias dl='docker-compose -f /apps/docker-compose.yml'
-alias dop='doppler'
-alias doplocal="doppler --api-host https://api.dopplerlocal.com:3030 --no-verify-tls"
-alias dse='~/work/cli/doppler secrets edit'
-alias dss='doppler secrets set'
 alias pgbak='~/work/backup-pg.sh'
 alias pgres='~/work/restore-pg.sh'
 alias pgls='ll /home/andre/work/docker/pgdatabackup/'
 alias pgcd='cd /home/andre/work/docker/pgdatabackup/'
 alias houston='HOST="https://houston.doppler.team" /home/andre/work/houston/scripts/houston.sh'
 alias houston-staging='HOST="https://houston.staging.doppler.team" /home/andre/work/houston/scripts/houston.sh'
-
-if [ `uname` = 'Darwin' ]
-then
-  alias trackpad='osascript ~/.dotfiles/bin/toggle-internal-trackpad.scpt'
-  alias o='open .'
-  alias pubkey="more ~/.ssh/id_rsa.pub | pbcopy | echo '=> Public key copied to pasteboard.'"
-  alias curdir="pwd | tr -d '\n' | pbcopy"
-else
-  alias o='explorer.exe .'
-fi
