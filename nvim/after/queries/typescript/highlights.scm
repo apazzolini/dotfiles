3702db00964e58ba9619eef82880d4f7263ccef3
Andre Azzolini <andre@azzolini.io>
2024-09-04 11:12:21 -0600

    highlight property fns

--- nvim/after/queries/typescript/highlights.scm
+++ nvim/after/queries/typescript/highlights.scm
@@ -1,2 +1,5 @@
 ; extends
 
+(public_field_definition name: (private_property_identifier) @function value: (arrow_function))
+(public_field_definition name: (property_identifier) @function value: (arrow_function))
+
