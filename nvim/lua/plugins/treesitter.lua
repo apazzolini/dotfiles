f2366eb13dcfdb7fcd19d85b5c9354b384d6b167
Andre Azzolini <andre@azzolini.io>
2024-12-09 14:45:46 -0700

    yati

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -17,9 +17,9 @@ return {
         'kana/vim-textobj-entire',
       },
     },
-    -- {
-    --   'yioneko/nvim-yati',
-    -- },
+    {
+      'yioneko/nvim-yati',
+    },
   },
   config = function()
     require('nvim-treesitter.configs').setup({
@@ -57,23 +57,12 @@ return {
       },
       indent = {
         enable = true,
-        -- disable = { 'javascript', 'typescript', 'jsx', 'tsx' },
+        disable = { 'javascript', 'typescript', 'jsx', 'tsx' },
       },
-      -- yati = {
-      --   enable = { 'javascript', 'typescript', 'jsx', 'tsx' },
-      --   -- Disable by languages, see `Supported languages`
-      --   disable = { 'python', 'lua', 'html', 'json', 'css' },
-      --
-      --   -- Whether to enable lazy mode (recommend to enable this if bad indent happens frequently)
-      --   default_lazy = true,
-      --
-      --   -- Determine the fallback method used when we cannot calculate indent by tree-sitter
-      --   --   "auto": fallback to vim auto indent
-      --   --   "asis": use current indent as-is
-      --   --   "cindent": see `:h cindent()`
-      --   -- Or a custom function return the final indent result.
-      --   default_fallback = 'auto',
-      -- },
+      yati = {
+        suppress_conflict_warning = true,
+        enable = { 'javascript', 'typescript', 'jsx', 'tsx' },
+      },
       highlight = {
         enable = true,
       },
