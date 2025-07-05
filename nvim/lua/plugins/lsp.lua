6e87b3dd16433394fae2fa6893db394f2b1121d6
Andre Azzolini <andre@azzolini.io>
2025-07-05 11:38:33 -0600

    font, lsp rename

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -181,6 +181,7 @@ return {
         vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
         vim.keymap.set('n', 'gh', vim.lsp.buf.hover, opts)
         vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, opts)
+        vim.keymap.set('n', '<leader>cr', vim.lsp.buf.rename, opts)
 
         vim.keymap.set('n', '<cr>', function()
           vim.cmd('noh')
