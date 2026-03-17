b66c2717f2633e16cbe704271ac449ba1693cf7e
Andre Azzolini <andre@azzolini.io>
2026-03-17 10:23:09 -0600

    updates

--- link.sh
+++ link.sh
@@ -38,6 +38,8 @@ function env_setup {
   mkdir -p ~/.config/opencode
   link opencode/opencode.jsonc ~/.config/opencode/
   link opencode/tui.jsonc ~/.config/opencode/
+  link opencode/skills ~/.config/opencode/
+  link opencode/themes ~/.config/opencode/
 }
 
 env_setup
