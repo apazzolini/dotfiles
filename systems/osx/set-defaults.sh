c9d0b6ccec2edd6d7f9e9f8275333ff7fc593215
Andre Azzolini <andre@azzolini.io>
2026-04-09 10:39:06 -0600

    icon spacing

--- systems/osx/set-defaults.sh
+++ systems/osx/set-defaults.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Icon spacing in system tray
-defaults -currentHost write -globalDomain NSStatusItemSpacing -int 2
-defaults -currentHost write -globalDomain NSStatusItemSelectionPadding -int 2
+defaults -currentHost write -globalDomain NSStatusItemSpacing -int 12
+defaults -currentHost write -globalDomain NSStatusItemSelectionPadding -int 6
 
 # Enable full keyboard access for all controls (e.g. enable Tab in modal dialogs)
 defaults write NSGlobalDomain AppleKeyboardUIMode -int 3
